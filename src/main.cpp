5449882c757468e39ef7aee0d54f8cb2a717f444
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-04-17 07:52:01 +0500

    Text...

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <raylib.h>
 
 int main(int argc, char const* argv[]) {
-	const int screenWidth = 800;
-    const int screenHeight = 450;
+	const int screenWidth = 1280;
+    const int screenHeight = 800;
 
     InitWindow(screenWidth, screenHeight, "SJ's Dot-Reaction");
     
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Test", 10, 10, 20, DARKGRAY);
+        DrawText("SJ's Dot-Reaction", 10, 10, 20, DARKGRAY);
         EndDrawing();
     }
     
